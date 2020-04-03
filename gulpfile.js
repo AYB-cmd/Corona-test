937fdffc3dd9d6840903ed6bad2afe2907b77a53
AYB-cmd <aeljelladi@gmail.com>
2020-04-03 07:37:51 +0100

    footer section

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,17 @@ var gulp = require("gulp"),
     uglify = require('gulp-uglify'),
     jshint = require('gulp-jshint'),
     imagemin = require('gulp-imagemin'),
-    connect = require('gulp-connect');
-    
+    connect = require('gulp-connect'),
+    rename = require("gulp-rename");
+ 
+
+gulp.task("html", () => {
+    return gulp.src('./app/*.html')
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest("./dist"))
+    .pipe(connect.reload())
+})
+
 gulp.task("sass", () => {
     return gulp.src("./app/sass/*.scss")
     .pipe(sass().on('error',sass.logError))
@@ -22,18 +31,22 @@ gulp.task('scripts', () => {
         .pipe(jshint.reporter('default'))
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'))
+        .pipe(connect.reload())
 });
 
 gulp.task('images', () => {
     return gulp.src('app/img/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/img'))
+        .pipe(connect.reload())
 });
 
 gulp.task('watch', () => {
+    gulp.watch('app/*.html', gulp.series('html'));
     gulp.watch('app/sass/*.scss', gulp.series('sass'));
     gulp.watch('app/js/*.js', gulp.series('scripts'));
-    gulp.watch('app/img/*', gulp.series('images'));
+    gulp.watch('app/img/*', gulp.series('images'))
+   
 });
 
 gulp.task('connect', () => {
@@ -43,4 +56,4 @@ gulp.task('connect', () => {
     });
   });
 
-  gulp.task('default', gulp.parallel('sass', 'scripts', 'images', 'watch','connect'));
+  gulp.task('default', gulp.parallel('html','sass', 'scripts', 'images', 'watch','connect'));
