9d68f296ec8ab3bc31b31a9cb8f93613654dd620
AYB-cmd <aeljelladi@gmail.com>
2020-04-09 22:58:18 +0100

    ToDo html/css page

---
+++ Mini projects/ToDo/js/script.js
@@ -0,0 +1,15 @@
+// // var block = document.getElementById('block');
+// // var title = document.getElementById('title');
+// // var card = document.getElementById('card');
+// // var todo =  document.getElementById('ToDO');
+
+// var item = document.createElement('li');
+// item.textContent = 'its working'
+
+// var list = document.querySelector('ul');
+// list.appendChild(item)
+// while (let i == 2, i > 1 ) {
+//     i--
+//     list.removeChild(list.firstChild)
+// }
+
--- gulpfile.js
+++ gulpfile.js
@@ -11,76 +11,82 @@ var gulp = require("gulp"),
     gulpif = require('gulp-if'),
     autoprefixer = require('autoprefixer'),
     inject = require('gulp-inject'),
+    injectPartials = require('gulp-inject-partials'),
     useref = require('gulp-useref'),
     postcss = require('postcss'),
     htmlmin = require('gulp-htmlmin');
     
-// gulp.task('useref', () => {
-//     var prefix = [
-//         autoprefixer({
-//             browsers: 'last 10 version'
-//         }),
-//     wring
-//     ];
+gulp.task('useref', () => {
+    var prefix = [
+        autoprefixer({
+            browsers: 'last 10 version'
+        }),
+    wring
+    ];
 
-//     return gulp.src('app/*.html')
-//     .pipe(useref())
-//     .pipe(htmlmin({
-//         collapseWhitespace: treu
-//     }))
-//     .pipe(gulfif('script.js',uglify()))
-//     .pipe(gulpif('*.css',postcss(prefix)))
-//     .pipe(gulp.src('./app/conseil.html'))
-//     .pipe(inject(gulf.src(['./dist/css/*.css'])))
-//     .pipe(gulp.dest('./dist'))
-// })
-
-gulp.task("html", () => {
-    return gulp.src('./app/*.html')
-    // .pipe(inject(['./dist/css/*.css']))
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest("./dist"))
-    .pipe(connect.reload())
+    return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(htmlmin({
+        collapseWhitespace: treu
+    }))
+    .pipe(gulfif('script.js',uglify()))
+    .pipe(gulpif('*.css',postcss(prefix)))
+    .pipe(gulp.src('./app/conseil.html'))
+    .pipe(inject(gulf.src(['./dist/css/*.css'])))
+    .pipe(gulp.dest('./dist'))
 })
-
-gulp.task("sass", () => {
-    return gulp.src("./app/sass/*.scss")
-    .pipe(sass().on('error',sass.logError))
-    .pipe(myth())
-    .pipe(uglifycss())
-    .pipe(gulp.dest("./dist/css"))
-    .pipe(connect.reload())
-});
-
-gulp.task('scripts', () => {
-    return gulp.src('./app/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'))
-        .pipe(connect.reload())
-});
-
-gulp.task('images', () => {
-    return gulp.src('app/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/img'))
-        .pipe(connect.reload())
-});
-
-gulp.task('watch', () => {
-    gulp.watch('app/*.html', gulp.series('html'));
-    gulp.watch('app/sass/*.scss', gulp.series('sass'));
-    gulp.watch('app/js/*.js', gulp.series('scripts'));
-    gulp.watch('app/img/*', gulp.series('images'))
-   
-});
-
-gulp.task('connect', () => {
-    connect.server({
-      root: 'dist', 
-      livereload: true
-    });
+gulp.task('index', function () {
+    var target = gulp.src('./app/conseil.html');
+    var sources = gulp.src(['./app/partials/*.html','./dist/js/*.js', './dist/css/*.css'], {read: false});
+    return target.pipe(inject(sources))
+      .pipe(gulp.dest('./app'));
   });
 
-  gulp.task('default', gulp.parallel('html','sass', 'scripts', 'images', 'watch','connect'));
+// gulp.task("html", () => {
+//     return gulp.src('./app/*.html')
+//     .pipe(htmlmin({collapseWhitespace: true}))
+//     .pipe(gulp.dest("./dist"))
+//     .pipe(connect.reload())
+// })
+
+// gulp.task("sass", () => {
+//     return gulp.src("./app/sass/*.scss")
+//     .pipe(sass().on('error',sass.logError))
+//     .pipe(myth())
+//     .pipe(uglifycss())
+//     .pipe(gulp.dest("./dist/css"))
+//     .pipe(connect.reload())
+// });
+
+// gulp.task('scripts', () => {
+//     return gulp.src('./app/js/*.js')
+//         .pipe(jshint())
+//         .pipe(jshint.reporter('default'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('./dist/js'))
+//         .pipe(connect.reload())
+// });
+
+// gulp.task('images', () => {
+//     return gulp.src('app/img/**/*')
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('./dist/img'))
+//         .pipe(connect.reload())
+// });
+
+// gulp.task('watch', () => {
+//     gulp.watch('app/*.html', gulp.series('html'));
+//     gulp.watch('app/sass/*.scss', gulp.series('sass'));
+//     gulp.watch('app/js/*.js', gulp.series('scripts'));
+//     gulp.watch('app/img/*', gulp.series('images'))
+   
+// });
+
+// gulp.task('connect', () => {
+//     connect.server({
+//       root: 'dist', 
+//       livereload: true
+//     });
+//   });
+
+//   gulp.task('default', gulp.parallel('html','sass', 'scripts', 'images', 'watch','connect'));
