24d7481972a9d690596c5d35fdc67cd11cb0277f
AYB-cmd <aeljelladi@gmail.com>
2020-05-10 10:06:08 +0000

    questions

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,14 @@
 const questionner = [
-    
-        {"Q" : "Pensez-vous avoir ou avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?",
-         "R" : "OUI NON" }
-        
-    
+        {
+            "Q" : "Pensez-vous avoir ou avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?",
+         "input1" : 0,
+        "input2" : 1 
+    },
+        {
+                "Q" : ""
+    }
+
+            
 ]
 
 
@@ -17,18 +22,47 @@ var question = document.querySelector('.question');
 var reponse = document.querySelector('.reponse');
 
 counter = 0;
-
-function getQst() {
-    score.innerHTML = `${counter}/23`
-    question.innerHTML = '';
-    var Q = document.createElement('h2')
-    Q.textContent = questionner[0].Q;
-    question.appendChild(Q)
-    // reponse.innerHTML = '';
-    // var R = document.createElement()
+questionNmbr = 0
+function startTest() {
+    getQst()
+    getRps()
     progressBar();
     style();
+    
 }
+
+function getQst(){
+    score.innerHTML = `${counter}/23`;
+    question.innerHTML = '';
+    var Q = document.createElement('h2');
+    Q.textContent = questionner[0].Q;
+    question.appendChild(Q);
+}
+function getRps() {
+    reponse.innerHTML = '';
+    var label = document.createElement('label')
+    label.textContent = "NON"
+    reponse.appendChild(label);
+    var R1 = document.createElement('input');
+    R1.value = questionner[0].input1;
+    R1.type = 'radio';
+    reponse.appendChild(R1); 
+    var label = document.createElement('label')
+    label.textContent = "OUI"
+    reponse.appendChild(label);
+    var R2 = document.createElement('input');
+    R2.value = questionner[0].input2;
+    R2.type = 'radio';
+    reponse.appendChild(R2);
+    // var R2 = document.createElement('input');
+    
+    // R2.value = questionner[0].input1;
+    // R2.type = 'checkbox';
+    // reponse.firstChild.appendChild(R1); 
+    // reponse.childElementCount(1).appendChild(R2);
+
+}
+
 
 function progressBar() {
     if (counter == 0){
@@ -42,5 +76,5 @@ function progressBar() {
 function style() {
     contentAria.style.backgroundColor = '#FFF'
 }
-start.addEventListener('click',getQst)
+start.addEventListener('click',startTest)
 
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,27 @@
 const questionner = [
-    
-        {"Q" : "Pensez-vous avoir ou avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?",
-         "R" : "OUI NON" }
-        
-    
+    {
+        "Q": "Pensez-vous avoir ou avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?",
+        "input1": 0,
+        "input2": 1
+    },
+    {
+        "Q": "Que pensez-vous de votre corps?"
+    },
+    {
+        "Q": " Quel est votre âge ? Ceci, afin de calculerun facteur de risque spécifique. "
+    },
+    {
+        "Q": ""
+    }, {
+        "Q": ""
+    }, {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+
+
 ]
 
 
@@ -16,31 +34,66 @@ var score = document.querySelector('.index');
 var question = document.querySelector('.question');
 var reponse = document.querySelector('.reponse');
 
-counter = 0;
+counter = -1;
 
-function getQst() {
-    score.innerHTML = `${counter}/23`
-    question.innerHTML = '';
-    var Q = document.createElement('h2')
-    Q.textContent = questionner[0].Q;
-    question.appendChild(Q)
-    // reponse.innerHTML = '';
-    // var R = document.createElement()
+function startTest() {
     progressBar();
+    getQst()
+    // getRps()
+    
     style();
+
 }
 
+function getQst() {
+    if (counter >= 0) {
+        question.innerHTML = '';
+        var Q = document.createElement('h2');
+        Q.textContent = questionner[counter].Q;
+        question.appendChild(Q);
+    }
+   
+}
+function getRps() {
+    reponse.innerHTML = '';
+    var label = document.createElement('label')
+    label.textContent = "NON"
+    reponse.appendChild(label);
+    var R1 = document.createElement('input');
+    R1.value = questionner[0].input1;
+    R1.type = 'radio';
+    reponse.appendChild(R1);
+    var label = document.createElement('label')
+    label.textContent = "OUI"
+    reponse.appendChild(label);
+    var R2 = document.createElement('input');
+    R2.value = questionner[0].input2;
+    R2.type = 'radio';
+    reponse.appendChild(R2);
+    // var R2 = document.createElement('input');
+
+    // R2.value = questionner[0].input1;
+    // R2.type = 'checkbox';
+    // reponse.firstChild.appendChild(R1); 
+    // reponse.childElementCount(1).appendChild(R2);
+
+}
+
+function questionNmr() {
+    score.innerHTML = `${counter+1}/23`;
+}
 function progressBar() {
-    if (counter == 0){
-    progress.style.width = '50%';
-    }else{
-      progress.style.width =`${50 + (50/23)*counter}%`; 
+    if (counter == 0) {
+        progress.style.width = '50%';
+    } else {
+        progress.style.width = `${50 + (50 / 23) * counter}%`;
     }
     counter++;
+    questionNmr()
 }
 
 function style() {
     contentAria.style.backgroundColor = '#FFF'
 }
-start.addEventListener('click',getQst)
+start.addEventListener('click', startTest)
 
