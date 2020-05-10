47acdb5843441a333bf553b7a775b9b713ed2952
AYB-cmd <aeljelladi@gmail.com>
2020-05-10 11:25:31 +0000

    prgressBar counter

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,6 +20,54 @@ const questionner = [
     {
         "Q": ""
     },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
+    {
+        "Q": ""
+    },
 
 
 ]
@@ -39,54 +87,61 @@ counter = -1;
 function startTest() {
     progressBar();
     getQst()
-    // getRps()
-    
+    getRps()
+
     style();
 
 }
 
 function getQst() {
-    if (counter >= 0) {
+    if (counter == 0) {
         question.innerHTML = '';
         var Q = document.createElement('h2');
         Q.textContent = questionner[counter].Q;
         question.appendChild(Q);
     }
-   
+
 }
 function getRps() {
-    reponse.innerHTML = '';
-    var label = document.createElement('label')
-    label.textContent = "NON"
-    reponse.appendChild(label);
-    var R1 = document.createElement('input');
-    R1.value = questionner[0].input1;
-    R1.type = 'radio';
-    reponse.appendChild(R1);
-    var label = document.createElement('label')
-    label.textContent = "OUI"
-    reponse.appendChild(label);
-    var R2 = document.createElement('input');
-    R2.value = questionner[0].input2;
-    R2.type = 'radio';
-    reponse.appendChild(R2);
-    // var R2 = document.createElement('input');
+    if (counter == 0) {
+        reponse.innerHTML = '';
+        var label = document.createElement('label');
+        label.setAttribute('class', 'label1')
+        label.textContent = "NON"
+        reponse.appendChild(label);
+        var label1 = document.querySelector('.label1');
+        var R1 = document.createElement('input');
+        R1.value = questionner[counter].input1;
+        R1.type = 'radio';
+        R1.name = "Q1"
+        label1.appendChild(R1);
 
-    // R2.value = questionner[0].input1;
-    // R2.type = 'checkbox';
-    // reponse.firstChild.appendChild(R1); 
-    // reponse.childElementCount(1).appendChild(R2);
+        var label = document.createElement('label');
+        label.setAttribute('class', 'label2')
+        label.textContent = "OUI"
+        reponse.appendChild(label);
+        var label2 = document.querySelector('.label2');
+        var R2 = document.createElement('input');
+        R2.value = questionner[counter].input1;
+        R2.type = 'radio';
+        R2.name = "Q1"
+        label2.appendChild(R2);
+
+       
+    } 
 
 }
 
 function questionNmr() {
-    score.innerHTML = `${counter+1}/23`;
+    score.innerHTML = `${counter + 1}/23`;
 }
 function progressBar() {
-    if (counter == 0) {
+    if (counter <= 0) {
         progress.style.width = '50%';
-    } else {
-        progress.style.width = `${50 + (50 / 23) * counter}%`;
+    } if (counter + 1 >= 1 & counter + 1 <= 22 ) {
+        progress.style.width = `${50 + (50 / 21) * counter}%`;
+    }if (counter > 20){
+       start.removeEventListener('click', startTest);
     }
     counter++;
     questionNmr()
