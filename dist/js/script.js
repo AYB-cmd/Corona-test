b1e45d5c4223ddb5a1cc30e284263332cd58756e
AYB-cmd <aeljelladi@gmail.com>
2020-05-13 15:59:56 +0000

    capture data

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,7 +6,27 @@ const questionner = [
         "input": "input",
         "type": "radio",
         "value": [1, 2],
-        "name": "Q1",
+        "name": "Q0",
+        "R": []
+    },
+    {
+        "Q": "Pensez-vous avoir ou avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?",
+        "element": ["label"],
+        "text": ["age"],
+        "input": "input",
+        "type": "text",
+        "value": " ",
+        "name": "Q0",
+        "R": []
+    },
+    {
+        "Q": "Pensez-vous avoir ou avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?",
+        "element": ["label", "label"],
+        "text": ["NON", "OUI"],
+        "input": "input",
+        "type": "radio",
+        "value": [1, 2],
+        "name": "Q0",
         "R": []
     },
     {
@@ -16,107 +36,62 @@ const questionner = [
         "input": "input",
         "type": "radio",
         "value": [1, 2, 1, 1],
-        "name": "Q2",
-        "R": ""
+        "name": "Q1",
+        "R": []
     },
-    {
-        "Q": " Quel est votre âge ? Ceci, afin de calculerun facteur de risque spécifique. ",
-        "element": ["label"],
-        "text": ["Ans"],
-        "input": "input",
-        "type": "text",
-        "value": [],
-        "name": "Q3",
-        "R": ""
-    },
-    {
-        "Q": " Lgana hahowa  ",
-        "element": ["label",],
-        "text": ["Ans"],
-        "input": "input",
-        "type": "text",
-        "value": [],
-        "name": "Q3",
-        "R": ""
-    },
-
-
 ];
 
-
-
-
-
-var inpt = document.querySelectorAll('input')
 var contentAria = document.getElementById('terminal');
 var progress = document.getElementById('progress');
 var btnAria = document.getElementById('bot');
 var startBtn = document.getElementById('start');
 var backBtn = document.getElementById('backBtn');
 var Nmr = document.querySelector('.index');
-var question = document.querySelector('question');
+var question = document.querySelector('.question');
 var reponse = document.querySelector('.reponse');
 var counter = -1;
-
-question.textContent
-
-function plus(nmrOne,nmrTwo) {
-    nmrOne + nmrTwo
-}
-
-plus()
-
-
+var result = [];
 
 // buttons
 startBtn.addEventListener('click', startTest)
 backBtn.addEventListener('click', back)
-
-
-// function Form(e) {
-//     var r = document.getElementsByTagName('input')
-//     if (counter >= 0) {
-//         startBtn.setAttribute('form', 'FORM')
-
-//         e.preventDefualt();  
-//     }
-
-// }
-
-// function getReponse(){
-//     inpt.forEach(element => {
-//         if (element.checked == true) {
-//             var n = element.value
-//             result.push(n)
-//             return result;
-
-//         }
-//     }, counter--);
-// }
+reponse.addEventListener('submit',e =>{e.preventDefault()})
 
 function startTest() {
+    getRps()
     counter++
-    
-    questionNmrP()
+    questionNmrP();
     progressBarP();
     postQst();
     postRps();
     style();
     BtnChanges();
-    getRps()
-    // startBtn.addEventListener('click',Next)
-    // startBtn.removeEventListener('click',startTest)
-}
-// function Next() {
+};
 
-//     counter++
-//     questionNmrP()
-//     progressBarP();
-//     postQst();
-//     postRps();
-//     style();
-//     BtnChanges();
-// }
+function getRps() {
+    if (counter >= 0) {
+        var input = document.querySelectorAll(".tst")
+        var r = questionner[counter].R
+        for (let i = 0; i < input.length; i++) {
+            const element = input[i];
+            if (element.checked || element.type == "text") {
+                var m = element.value;
+                r.push(m);
+            }
+        }if (r.length == 0){
+            counter--
+            var q = questionner[counter];
+            if(q.type == 'radio'){
+                alert('you have to select one')
+            }
+            // }else{
+            //     alert('you have to fill it')
+            // }
+            
+        }
+    }
+};
+
 function back() {
     counter--;
     questionNmrP();
@@ -128,14 +103,10 @@ function back() {
 function questionNmrP() {
     Nmr.innerHTML = `${counter + 1}/23`;
 }
+
 function progressBarP() {
-
-
-
     if (counter <= 0) {
         progress.style.width = '50%';
-    }
-    if (counter == 0) {
         backBtn.style.display = 'none'
     } if (counter >= 1 & counter <= 22) {
         progress.style.width = `${50 + (50 / 22) * counter}%`;
@@ -143,16 +114,6 @@ function progressBarP() {
         startBtn.removeEventListener('click', startTest);
     }
 }
-
-
-
-
-
-
-
-
-
-
 
 function style() {
     contentAria.style.backgroundColor = '#FFF'
@@ -163,7 +124,7 @@ function BtnChanges() {
         btnAria.style.width = '100%';
         btnAria.style.display = 'flex';
         btnAria.style.justifyContent = 'flex-end';
-        startBtn.type = 'submit'
+        // startBtn.setAttribute('form', 'FORM')
         startBtn.textContent = 'question suivante';
         startBtn.style.width = '45%';
         startBtn.style.margin = '1em 0';
@@ -173,9 +134,7 @@ function BtnChanges() {
         backBtn.textContent = "question d'avant";
         backBtn.style.display = 'block';
         btnAria.appendChild(backBtn);
-
     }
-
 }
 
 // postINFO
@@ -186,6 +145,7 @@ function postQst() {
         var Q = document.createElement('h2');
         Q.textContent = questionner[counter].Q;
         question.appendChild(Q);
+
     }
 }
 
@@ -200,25 +160,14 @@ function postRps() {
         var selected = document.getElementById(`${i}`);
         var r = document.createElement(questionner[counter].input);
         r.type = q.type;
-        r.value= q.value[i] ;
+        r.value = q.value[i];
         r.name = q.name;
-        r.required = true
+        r.required = true;
         selected.appendChild(r);
+        selected.lastChild.setAttribute('class', 'tst')
     }
 
 }
-
-var result = [];
-
-    function getRps() {
-        for (let i = 0; i < inpt.length; i++) {
-            const element = inpt[i];
-            if (element.checked) {
-            var m = element.value;
-                result.push(m)
-            }   
-        }
-    }
 
 
 
